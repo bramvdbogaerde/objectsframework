c092cff9783ef476017a18d760cc783a3142ae8a
Bram Vandenbogaerde <contact@bramvdb.com>
2015-01-04 10:06:53 +0100

    update example to demonstrate the duality of the objectsframework::object (speaking in MVC language todo is a controller and model at the same time)

--- examples/todo-app/db.rb
+++ examples/todo-app/db.rb
@@ -1,9 +1,2 @@
 MongoMapper.connection = Mongo::Connection.new
 MongoMapper.database = "objectsframework-todo-example"
-
-module Models
-  class Todo
-    include MongoMapper::Document
-    key :content
-  end
-end
--- examples/todo-app/todo.rb
+++ examples/todo-app/todo.rb
@@ -1,10 +1,13 @@
 class Todo < ObjectsFramework::Object
+  include MongoMapper::Document
+  key :content
+
   def post_save
     data = JSON.parse(request.body.read)
-    response.write Models::Todo.create({:content => data["content"]}).to_json
+    response.write Todo.create({:content => data["content"]}).to_json
   end
 
   def get_all
-    response.write Models::Todo.all.to_json
+    response.write Todo.all.to_json
   end
 end
